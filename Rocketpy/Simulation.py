eb18ced1a0027b8887fd4d58c0f24ee38e63545d
ArthurJWH <167456467+ArthurJWH@users.noreply.github.com>
2024-11-11 20:23:01 -0500

    Add files via upload

---
+++ Rocketpy/Simulation.py
@@ -0,0 +1,15 @@
+from rocketpy import Flight
+
+import DefiningEnvironment
+import BuildingRocket
+
+test_flight = Flight(
+    rocket=BuildingRocket.rocket,
+    environment=DefiningEnvironment.env,
+    rail_length=5.2, # metres
+    inclination=85, # degrees
+    heading=0 # degrees
+    )
+
+# test_flight.info()
+# test_flight.export_kml("trajectory.kml")
