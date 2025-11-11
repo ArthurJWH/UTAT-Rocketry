75e4eb3a761354e08b2d8a622602c42dc4a569b1
Nadiiim117 <119913905+Nadiiim117@users.noreply.github.com>
2025-11-10 21:35:21 -0500

    Update environment parameters for flight simulation

--- Rocketpy/Discovery/DefiningEnvironment.py
+++ Rocketpy/Discovery/DefiningEnvironment.py
@@ -3,20 +3,19 @@ from rocketpy import Environment
 """ Defining the Environment """
 
 """ Location """
-env = Environment(latitude=32.990254, longitude=-106.974998, elevation=1400)
+env = Environment(latitude=47.96527, longitude=-81.87412, elevation=1382.9)
 
 """ Time """
 import datetime
 
-tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+flight_day= datetime.date(2026, 8, 24)
 
 env.set_date(
-    (tomorrow.year, tomorrow.month, tomorrow.day, 12)
+    (flight_day.year, flight_day.month, flight_day.day, 12)
 )  # Hour given in UTC time
 
 """ Atmospheric Model """
 env.set_atmospheric_model(
-    type="Forecast", file="GFS"  # "custom_atmosphere", wind_v, wind_u
-)
+    type="custom_atmosphere", wind_u = 3, wind_v = 4)  # "custom_atmosphere", wind_v, wind_u
 
 # env.info()
