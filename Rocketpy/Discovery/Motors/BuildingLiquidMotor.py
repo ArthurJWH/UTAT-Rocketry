aa19cfb24e1c5ca6ea313679839ebe5541a0251b
Nadiiim117 <119913905+Nadiiim117@users.noreply.github.com>
2026-07-08 15:07:59 +0300

    Refactor liquid motor configuration and parameters
    Updated fuel and oxidizer tank geometries, adjusted mass flow rates, and modified motor parameters.

--- Rocketpy/Discovery/Motors/BuildingLiquidMotor.py
+++ Rocketpy/Discovery/Motors/BuildingLiquidMotor.py
@@ -7,30 +7,28 @@ oxidizer_gas = Fluid(name="Gas N2O", density=1.9277)  # kg/m3
 
 fuel_liq = Fluid(name="Liquid ethanol", density=789)  # kg/m3
 
-fuel_gas = Fluid(name="Gas ethanol", density=1.59)  # kg/m3
+fuel_gas = Fluid(name="Nitrogen Gas", density=55)  # kg/m3
 
 """ Creating the tank """
-tanks_geometry = CylindricalTank(
-    radius=0.1, height=1.2, spherical_caps=True  # meters  # meters
+fuel_tank_geometry = CylindricalTank(
+    radius=0.076, height=1.24, spherical_caps=False  # meters  # meters
 )
 
+oxidizer_tank_geometry = CylindricalTank(
+    radius=0.076, height=0.64, spherical_caps=False  # meters
+)
 
 """ Defining flow rate of the oxidizer """
 from math import exp
 
-
-def mass_flow_rate_function(t):  # load .csv table of time and flow rate
-    return 32 / 3 * exp(-0.25 * t)
-
-
 oxidizer_tank = MassFlowRateBasedTank(
     name="oxidizer tank",
-    geometry=tanks_geometry,
-    flux_time=5,  # seconds
-    initial_liquid_mass=32,  # kg
+    geometry=oxidizer_tank_geometry,
+    flux_time=4.58,  # seconds
+    initial_liquid_mass=7.01,  # kg
     initial_gas_mass=0.01,  # kg
     liquid_mass_flow_rate_in=0,
-    liquid_mass_flow_rate_out=mass_flow_rate_function,
+    liquid_mass_flow_rate_out= 7.01/4.58,
     gas_mass_flow_rate_in=0,
     gas_mass_flow_rate_out=0,
     liquid=oxidizer_liquid,
@@ -39,25 +37,16 @@ oxidizer_tank = MassFlowRateBasedTank(
 
 """ Defining the flow rate of the fuel """
 
-
-def liquid_mass_flow_rate_function(t):  # load .csv table of time and flow rate
-    return 21 / 3 * exp(-0.25 * t)
-
-
-def gas_mass_flow_rate_function(t):  # load .csv table of time and flow rate
-    return 0.01 / 3 * exp(-0.25 * t)
-
-
 fuel_tank = MassFlowRateBasedTank(
     name="fuel tank",
-    geometry=tanks_geometry,
-    flux_time=5,  # seconds
-    initial_liquid_mass=21,  # kg
-    initial_gas_mass=0.01,  # kg
+    geometry=fuel_tank_geometry,
+    flux_time=4.58,  # seconds
+    initial_liquid_mass= 2.42,  # kg
+    initial_gas_mass= 1,  # kg
     liquid_mass_flow_rate_in=0,
-    liquid_mass_flow_rate_out=liquid_mass_flow_rate_function,
+    liquid_mass_flow_rate_out= 2.42/4.58,
     gas_mass_flow_rate_in=0,
-    gas_mass_flow_rate_out=gas_mass_flow_rate_function,
+    gas_mass_flow_rate_out=0,
     liquid=fuel_liq,
     gas=fuel_gas,
 )
@@ -71,26 +60,27 @@ def thrust_curve_function(t):  # load .csv table of time and thrust
 
 """ Assembling liquid motor """
 liquid_motor = LiquidMotor(
-    thrust_source=thrust_curve_function,
-    dry_mass=2,  # kg
-    dry_inertia=(0.125, 0.125, 0.002),  # kg/m2
-    nozzle_radius=0.075,
-    center_of_dry_mass_position=1.75,
+   thrust_source="enginefilev4.csv",
+    dry_mass=20.088,
+    dry_inertia=(0.125, 0.125, 0.002),
+    center_of_dry_mass_position= 1.074,
+    nozzle_radius=0.043,
+    burn_time=4.58,
     nozzle_position=0,
-    burn_time=5,  # seconds
+    interpolation_method="linear",
     coordinate_system_orientation="nozzle_to_combustion_chamber",
 )
 
 """ Add tanks to the motor """
 liquid_motor.add_tank(
     tank=oxidizer_tank,
-    position=1.0,  # meters
+    position=1.22,  # meters
 )
 
 liquid_motor.add_tank(
     tank=fuel_tank,
-    position=2.5,  # meters
+    position=2.45,  # meters
 )
 
-# liquid_motor.info()
-# liquid_motor.draw()
+#liquid_motor.info()
+#liquid_motor.draw()
