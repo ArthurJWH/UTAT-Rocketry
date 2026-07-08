1ae3d5fd8771f195fc67b15d59e7df24d44645ea
Nadiiim117 <119913905+Nadiiim117@users.noreply.github.com>
2026-07-08 15:03:15 +0300

    Adjust chamber dimensions for GenericMotor

--- Rocketpy/Discovery/Motors/BuildingGenericMotor.py
+++ Rocketpy/Discovery/Motors/BuildingGenericMotor.py
@@ -10,8 +10,8 @@ def thrust_curve_function(t):  # load .csv table of time and thrust
 generic_motor = GenericMotor(
     thrust_source="Rocketpy/Discovery/Curves/enginefilev4.csv",
     chamber_radius=0.259,
-    chamber_height=0.98,
-    chamber_position=0.49,
+    chamber_height=0.198,
+    chamber_position=0.099,
     dry_mass=9.375,
     propellant_initial_mass=0,
     dry_inertia=(0.125, 0.125, 0.002),
